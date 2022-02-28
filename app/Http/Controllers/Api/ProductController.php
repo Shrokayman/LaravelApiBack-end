083d44d48250ef13be91b8d5be274697fbb36c3f
MohamedSaad0 <mohamed_saad.012@yahoo.com>
2022-02-28 03:36:00 +0200

    Creating Product API

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\StoreProductRequest;
-use App\Http\Requests\UpdateProductRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Models\Product;
+    
+// use App\Http\Requests\StoreProductRequest;
+// use App\Http\Requests\UpdateProductRequest;
 
 class ProductController extends Controller
 {
@@ -16,6 +19,7 @@ class ProductController extends Controller
     public function index()
     {
         //
+        return response()->json(Product::all(), 200);
     }
 
     /**
@@ -25,62 +29,104 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        // Return create view
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreProductRequest  $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreProductRequest $request)
+    public function store(Request $request)
     {
         //
+        $product = new Product;
+        $product->name=$request->name;
+        $product->image=$request->image;
+        $product->description=$request->description;
+        $product->price=$request->price;
+        $product->discount=$request->discount;
+        $product->average_rate=$request->average_rate;
+        $product->category_id=$request->category_id;
+        $product->brand_id=$request->brand_id;
+        $product->save();
+        return response()->json($product, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
+    public function show($id)
     {
         //
+        $product =  Product::find($id);
+        $categoryName = $product->category()->get()->first()->name;
+        $brandName = $product->brand()->get()->first()->name;
+        return array('product' => $product, 'categoryName' => $categoryName, 'brandName' => $brandName);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Product $product)
+    public function edit($id)
     {
-        //
+        $product= Product::find($id);
+        if(is_null($product)){
+            return response()->json(['message' =>'Product Not Found', 404]);
+        }
+        return response()->json($product::find($id), 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateProductRequest  $request
-     * @param  \App\Models\Product  $product
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(Request $request, $id)
     {
-        //
+
+        $product = Product::find($id);
+        if(is_null($product)){
+            return response()->json(['message' =>'Product Not Found' , 404]);
+        }
+
+        $product->name=$request->name;
+        $product->image=$request->image;
+        $product->description=$request->description;
+        $product->price=$request->price;
+        $product->discount=$request->discount;
+        $product->average_rate=$request->average_rate;
+        $categoryName = $product->category()->get()->first()->name;
+        $brandName = $product->brand()->get()->first()->name;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $product->save();
+        return response()->json(array('product' => $product, 'categoryName' => $categoryName, 'brandName' => $brandName), 200);
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        $product = Product::find($id);
+        if(is_null($product)){
+            return response()->json(['message' => 'Product Not Found', 404]);
+        }
+        Product::destroy($id);
+        return response()->json(['message'=>'Product Deleted', 204]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductController;
 
 
 /*
@@ -28,4 +29,29 @@ Route::put('users/{id}' ,[UserController::class , "update"]);
 
 // Show user by id
 Route::get('users/{id}' , [UserController::class , "show"]);
+
+
+
+////////////////////////////////////////////////////
+//                  Products                      //
+////////////////////////////////////////////////////
+
+// List
+Route::get('/products', [ProductController::class,'index']);
+
+// Create 
+// Route::get('/products/create', [ProductController::class,'create']);
+
+// Store
+Route::post('/products', [ProductController::class,'store']);
+
+// Show
+Route::get('/products/{id}',[ProductController::class,'show']);
+
+// Update
+Route::put('/products/{id}',[ProductController::class,'update']);
+
+// Delete
+Route::delete('/products/{id}',[ProductController::class,'destroy']);
+
 
