9315ae6dfb7f54942f20fe7e691dc2765ad17659
Mahmooud94 <mahmoud_gamal94@hotmail.com>
2022-03-05 23:37:05 +0200

    all in

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Models\Brand;
 use App\Models\Product;
 
 /*
@@ -44,11 +47,58 @@ Route::group(['middleware' => ['jwt.auth']], function () {
 
 //////////////////////// User ///////////////////////////////
 
-Route::post('register', [UserController::class, 'register']);
-Route::post('login', [UserController::class, 'login']);
 
-// Update user's data
-Route::put('users/{id}', [UserController::class, "update"]);
+Route::post('/register' , [UserController::class , 'register']);
+Route::post('/login' , [UserController::class , 'login']);
 
-// Show user by id
-Route::get('users/{id}', [UserController::class, "show"]);
+
+
+// Show All Users
+Route::get('users' , [UserController::class , "index"]);
+
+
+// Update user's data (Protected route : requires token)
+Route::put('users/{id}' ,[UserController::class , "update"]);
+
+// Show user by id (Protected route : requires token)
+Route::get('users/{id}' , [UserController::class , "show"]);
+
+
+// To get a new token after expiry of the token (Protected route : requires old token to renew it)
+Route::get('refresh' ,[UserController::class , "refresh"] );
+//
+
+// Delete User
+Route::delete('users/{id}' , [UserController::class , "destroy"]);
+
+
+Route::get('/brands', [BrandController::class, 'index']);
+Route::get('/brands/{id}', [BrandController::class, 'getProducts']);
+Route::post('/brands', [BrandController::class, 'store']);
+Route::delete('brands/{id}',[BrandController::class,'destroy']);
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('categories/{id}',[BrandController::class,'getProducts']);
+Route::post('categories',[CategoryController::class,'store']);
+Route::delete('categories/{id}',[CategoryController::class,'destroy']);
+
+///////////////////////////////////////////////////////////////////////
+//                         Products                                  //
+///////////////////////////////////////////////////////////////////////
+
+// List
+Route::get('/products', [ProductController::class,'index']);
+
+// Store
+Route::post('/products', [ProductController::class,'store']);
+
+// Show Single Product
+Route::get('/product/{id}',[ProductController::class,'show']);
+
+// Update
+Route::put('/products/{id}',[ProductController::class,'update']);
+
+// Delete
+Route::delete('/products/{id}',[ProductController::class,'destroy']);
+
+// Search -- Tested In Postman
+Route::get('/products/search/{name}',[ProductController::class,'search']);
